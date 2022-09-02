2885a4c78ba58dba12d3f0a928aeff9fd1e0f89f
jkarsenty <48349745+jkarsenty@users.noreply.github.com>
2022-09-02 12:34:25 +0200

    add cli users

--- cli/users.py
+++ cli/users.py
@@ -1,24 +1,75 @@
 import typer
+import requests
+from requests.structures import CaseInsensitiveDict
+
+URL_APP = "http://www.localhost:8000"
 
 app = typer.Typer()
 
 @app.command()
 def create_user(name:str, login:str, pwd:str):
-    print(f"name : {name}")
-    print(f"login : {login}")
-    print(f"pwd : {pwd}")
+    typer.echo(f"name : {name}")
+    typer.echo(f"login : {login}")
+    typer.echo(f"pwd : {pwd}")
+
+    endpoint = "users/create"
+    url = f"{URL_APP}/{endpoint}"
+    headers = CaseInsensitiveDict()
+    headers["accept"] = "application/json"
+    headers["Content-Type"] = "application/json"
+
+    data ={
+        "name": name,
+        "login": login,
+        "pwd": pwd
+    }
+
+    resp = requests.post(url, headers=headers, json=data)
+
+    print(resp.status_code)
+    print(resp.json())
+
 
 @app.command()
 def get_all_users():
-    print("All Users")
+    typer.echo("All Users")
+    
+    endpoint = "users/list"
+    url = f"{URL_APP}/{endpoint}"
+
+    resp = requests.get(url)
+
+    print(resp.status_code)
+    print(resp.json())
+
 
 @app.command()
 def get_one_user(user_id:int):
-    print(f"User {user_id}")
+    typer.echo(f"User {user_id}")
+
+    endpoint = "users/list"
+    url = f"{URL_APP}/{endpoint}/{user_id}"
+
+    resp = requests.get(url)
+
+    print(resp.status_code)
+    print(resp.json())
+
 
 @app.command()
 def delete_one_user(user_id:int):
-    print(f"User {user_id} deleted")
+    typer.echo(f"User {user_id} deleted")
+    
+    endpoint = "users/delete"
+    url = f"{URL_APP}/{endpoint}/{user_id}"
+
+    headers = CaseInsensitiveDict()
+    headers["accept"] = "*/*"
+
+
+    resp = requests.delete(url, headers=headers)
+
+    print(resp.status_code)
 
 
 if __name__ == "__main__":
